2839525f126f4d672b6c22b8fc04301372114f16
Meghana <meghana.vishwanat001@umb.edu>
2017-06-20 21:28:18 +0000

    update googlemap api

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(flash());
 //app.use(Promise);
-// seedDB();
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
